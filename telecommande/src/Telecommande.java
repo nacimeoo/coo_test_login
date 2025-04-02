f5a1013bb75924d3ec7a58ed0b8caa63d62fa217
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-02 09:02:30 +0200

    creation du squelette de la classe telecommande

---
+++ telecommande/src/Telecommande.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Telecommande {
+    private ArrayList<Lampe> lampes;
+
+    public Telecommande() {
+
+    }
+    public void ajouterLampe(Lampe l){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerLampe(int indiceLampe ){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void desactiverLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerTout(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public String toString(){
+        throw new Error (" code non ecrit ");
+    }
+
+
+
+
+
+}
