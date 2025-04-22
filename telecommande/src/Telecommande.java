e7c6d2bf5fa23d31f6f45533565fc4b42202386f
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-22 14:42:21 +0200

    modification de la class telecommande pour manipuler les chaine hif

--- telecommande/src/Telecommande.java
+++ telecommande/src/Telecommande.java
@@ -1,15 +1,22 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Telecommande {
     private final ArrayList<Lampe> lampes;
+    private final ArrayList<Hifi> hifi;
 
     public Telecommande() {
         this.lampes = new ArrayList<>();
+        this.hifi = new ArrayList<>();
     }
 
 
     public void ajouterLampe(Lampe l){
         lampes.add(l);
+    }
+
+    public void ajouterHifi(Hifi h){
+        hifi.add(h);
     }
 
     public void activerLampe(int indiceLampe ){
@@ -20,6 +27,14 @@ public class Telecommande {
         }
     }
 
+    public void activerHifi(int indiceHifi){
+        if (indiceHifi >= 0 && indiceHifi < hifi.size()){
+            hifi.get(indiceHifi).allumer();
+        }else{
+            System.out.println("Hifi exists pas");
+        }
+    }
+
     public void desactiverLampe(int indiceLampe){
         if(indiceLampe >= 0 && indiceLampe < lampes.size()) {
             lampes.get(indiceLampe).eteindre();
@@ -27,9 +42,21 @@ public class Telecommande {
             System.out.println("Lampe nexiste pas");
         }
     }
+
+    public void desactiverHifi(int indiceHifi){
+        if (indiceHifi >= 0 && indiceHifi < hifi.size()) {
+            hifi.get(indiceHifi).eteindre();
+        }else{
+            System.out.println("Hifi nexiste pas");
+        }
+    }
+
     public void activerTout(){
         for (Lampe l : lampes) {
             l.allumer();
+        }
+        for (Hifi h : hifi) {
+            h.allumer();
         }
     }
 
