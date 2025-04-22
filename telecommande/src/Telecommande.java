f609995bf13674b84d2699812e11689c0ca97160
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-22 15:17:51 +0200

    modif des class

--- telecommande/src/Telecommande.java
+++ telecommande/src/Telecommande.java
@@ -3,11 +3,9 @@ import java.util.ArrayList;
 
 public class Telecommande {
     private final ArrayList<Lampe> lampes;
-    private final ArrayList<Hifi> hifi;
 
     public Telecommande() {
         this.lampes = new ArrayList<>();
-        this.hifi = new ArrayList<>();
     }
 
 
@@ -15,9 +13,7 @@ public class Telecommande {
         lampes.add(l);
     }
 
-    public void ajouterHifi(Hifi h){
-        hifi.add(h);
-    }
+
 
     public void activerLampe(int indiceLampe ){
         if(indiceLampe >= 0 && indiceLampe < lampes.size()){
@@ -27,13 +23,7 @@ public class Telecommande {
         }
     }
 
-    public void activerHifi(int indiceHifi){
-        if (indiceHifi >= 0 && indiceHifi < hifi.size()){
-            hifi.get(indiceHifi).allumer();
-        }else{
-            System.out.println("Hifi exists pas");
-        }
-    }
+
 
     public void desactiverLampe(int indiceLampe){
         if(indiceLampe >= 0 && indiceLampe < lampes.size()) {
@@ -43,30 +33,19 @@ public class Telecommande {
         }
     }
 
-    public void desactiverHifi(int indiceHifi){
-        if (indiceHifi >= 0 && indiceHifi < hifi.size()) {
-            hifi.get(indiceHifi).eteindre();
-        }else{
-            System.out.println("Hifi nexiste pas");
-        }
-    }
+
 
     public void activerTout(){
         for (Lampe l : lampes) {
             l.allumer();
         }
-        for (Hifi h : hifi) {
-            h.allumer();
-        }
+
     }
 
     public String toString(){
         String res = "";
         for (Lampe l : lampes) {
             res += l.toString() + "\n";
-        }
-        for (Hifi h : hifi) {
-            res += h.toString() + "\n";
         }
         return res;
     }
