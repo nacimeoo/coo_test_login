cfb9677f583b0080ff79fc99b6e9c1d1a9215765
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-24 17:18:48 +0200

    ajout class chemineAdapter

--- telecommande/src/ChemineeAdapter.java
+++ telecommande/src/ChemineeAdapter.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class ChemineeAdapter implements Peripherique {
+    private Cheminee cheminee;
 
-public class ChemineeAdapter {
+    public ChemineeAdapter(Cheminee cheminee) {
+        this.cheminee = cheminee;
+    }
+
+    public void allumer() {
+        cheminee.changerIntensite(cheminee.getLumiere()+10);
+    }
+
+    public void eteindre(){
+        cheminee.changerIntensite(0);
+    }
+
+    public String toString() {
+        return cheminee.toString();
+    }
+
 }
