e9626789d7ee4bf81956f1ec083d381d51cc16e6
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-24 17:20:56 +0200

    ajout de test allumer et eteindre cheminee

--- telecommande/test/TelecommandeTest.java
+++ telecommande/test/TelecommandeTest.java
@@ -24,11 +24,23 @@ public class TelecommandeTest {
     @Test
     public void activation_pos0() {
         Telecommande t = new Telecommande();
+        Cheminee c = new Cheminee();
+        ChemineeAdapter c2 = new ChemineeAdapter(c);
         Hifi h = new Hifi();
         Lampe l = new Lampe("lampe1");
-        t.ajouter(h);
+        t.ajouter(c2);
         t.activer(0);
-        assertEquals("Hifi:10\n", t.toString());
+        assertEquals("cheminee: 10\n", t.toString());
+    }
+
+    @Test
+    public void eteindre_pos0(){
+        Telecommande t = new Telecommande();
+        Cheminee c = new Cheminee();
+        ChemineeAdapter c2 = new ChemineeAdapter(c);
+        t.ajouter(c2);
+        t.desactiverPeripherique(0);
+        assertEquals("cheminee: 0\n", t.toString());
     }
 
     @Test
