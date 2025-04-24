22cbe06bfbb4f87eb570b9d39be6ec8594b83ed5
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-04-24 15:37:24 +0200

    modification des methode de la class

--- telecommande/src/Telecommande.java
+++ telecommande/src/Telecommande.java
@@ -1,72 +1,49 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Telecommande {
-    private final ArrayList<Lampe> lampes;
-    private final ArrayList<Hifi> hifi;
+    private final ArrayList<Peripherique> peripherique;
 
     public Telecommande() {
-        this.lampes = new ArrayList<>();
-        this.hifi = new ArrayList<>();
+        this.peripherique = new ArrayList<>();
     }
 
 
-    public void ajouterLampe(Lampe l){
-        lampes.add(l);
+    public void ajouter(Peripherique p){
+        peripherique.add(p);
     }
 
-    public void ajouterHifi(Hifi h){
-        hifi.add(h);
-    }
 
-    public void activerLampe(int indiceLampe ){
-        if(indiceLampe >= 0 && indiceLampe < lampes.size()){
-            lampes.get(indiceLampe).allumer();
+    public void activer(int indicePeripherique ){
+        if(indicePeripherique >= 0 && indicePeripherique < peripherique.size()){
+            peripherique.get(indicePeripherique).allumer();
         }else{
-            System.out.println("Lampe exists pas");
+            System.out.println("peripherique exists pas");
         }
     }
 
-    public void activerHifi(int indiceHifi){
-        if (indiceHifi >= 0 && indiceHifi < hifi.size()){
-            hifi.get(indiceHifi).allumer();
+
+
+    public void desactiverPeripherique(int indicePeripherique){
+        if(indicePeripherique >= 0 && indicePeripherique < peripherique.size()) {
+            peripherique.get(indicePeripherique).eteindre();
         }else{
-            System.out.println("Hifi exists pas");
+            System.out.println("peripherique nexiste pas");
         }
     }
 
-    public void desactiverLampe(int indiceLampe){
-        if(indiceLampe >= 0 && indiceLampe < lampes.size()) {
-            lampes.get(indiceLampe).eteindre();
-        }else{
-            System.out.println("Lampe nexiste pas");
-        }
-    }
 
-    public void desactiverHifi(int indiceHifi){
-        if (indiceHifi >= 0 && indiceHifi < hifi.size()) {
-            hifi.get(indiceHifi).eteindre();
-        }else{
-            System.out.println("Hifi nexiste pas");
-        }
-    }
 
     public void activerTout(){
-        for (Lampe l : lampes) {
-            l.allumer();
+        for (Peripherique p : peripherique) {
+            p.allumer();
         }
-        for (Hifi h : hifi) {
-            h.allumer();
-        }
+
     }
 
     public String toString(){
         String res = "";
-        for (Lampe l : lampes) {
-            res += l.toString() + "\n";
-        }
-        for (Hifi h : hifi) {
-            res += h.toString() + "\n";
+        for (Peripherique p : peripherique) {
+            res += p.toString() + "\n";
         }
         return res;
     }
