9e0f4d770f582e6e368e4aecb7ec5b9cd1b408eb
Nacime Laghezali <Nacime.laghezali3@etu.univ-lorraine.fr>
2025-05-13 08:53:42 +0200

    ajout de la method eetreavant

--- TD11/PROJETCD/src/donnees/ComparateurArtiste.java
+++ TD11/PROJETCD/src/donnees/ComparateurArtiste.java
@@ -1,4 +1,7 @@
 package donnees;
 
-public class ComparateurArtiste {
+public class ComparateurArtiste implements ComparateurCd {
+    public boolean etreAvant(CD cd1, CD cd2) {
+        return cd1.getArtiste().compareTo(cd2.getArtiste())<0;
+    }
 }
